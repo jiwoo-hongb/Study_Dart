f9f6a7bb8ceaaf14bdb4916ddf1a6bf4e1759ad8
jwHong26 <150429157+jwHong26@users.noreply.github.com>
2024-11-05 20:03:14 +0900

    first ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 
+class Player {
+  String? name;
+  //? => name을 가질 수도 아닐 수도 있음
+  Player({required this.name});
+}
+
 void main() {
+  var jw = Player(name: "jw");
   runApp(App());
 }
 
@@ -9,15 +16,43 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      //MaterialApp vs ios 디자인 중 선택
-      home: Scaffold(
-          appBar: AppBar(
-            elevation: 10,
-            title: Text("hello flutter"),
-          ),
-          body: Center(
-            child: Text("hello world"),
+        //MaterialApp vs ios 디자인 중 선택
+        home: Scaffold(
+      backgroundColor: Color(0xFF181818),
+      body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 40), //수평으로 40의 간격 제공
+          child: Column(
+            children: [
+              SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Hey, Selena',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.8),
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              )
+            ],
           )),
-    );
+    ));
   }
 }
