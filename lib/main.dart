7f04a4727c0d8ab6b2103b53481d5c5ad89ce492
jwHong26 <jwhong48@gmail.com>
2024-11-06 20:23:50 +0900

    UI practice(ver 1)

--- lib/main.dart
+++ lib/main.dart
@@ -20,16 +20,21 @@ class App extends StatelessWidget {
         home: Scaffold(
       backgroundColor: Color(0xFF181818),
       body: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 40), //수평으로 40의 간격 제공
+          padding: EdgeInsets.symmetric(
+            horizontal: 40,
+            ), //수평으로 40의 간격 제공
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
                 height: 80,
               ),
               Row(
+                // 수평으로 놓고 싶을 때 Row의 mainAxis / 수직 = Row의 crossAxis
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   Column(
+                    // 수직으로 놓고 싶을 때 Column의 mainAxis / 수평 = Column의 crossAxis
                     crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
                       Text(
@@ -49,6 +54,47 @@ class App extends StatelessWidget {
                       ),
                     ],
                   )
+                ],
+              ),
+              SizedBox(
+                height: 120,
+              ),
+              Text(
+                "Total balance",
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.white.withOpacity(0.8),
+                ),
+              ),
+              Text(
+                "\$5 194 482",
+                style: TextStyle(
+                  fontSize: 48,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white.withOpacity(0.8),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(45),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 20, horizontal: 50,
+                      ),
+                      child: Text("Transfer",
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    )
+                  ),
                 ],
               )
             ],
